b218a576a804c065b1af09a2365758e4987703b5
root <root@DESKTOP-2KLJLBF.localdomain>
2020-03-16 17:14:19 -0400

    Journal entries on the DOM are now populated from the JSON server.

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,27 +1,17 @@
-// This is the original data. Can't Touch This.
-const journal = [
-    {
-        date: "2/26/2020",
-        concept: "HTML & CSS",
-        entry: "We worked on a travel brochure site as a team.",
-        mood: "Good"
-    },
-    {
-        date: "07/27/2025",
-        concept: "Javascript",
-        entry: "We talked about representing fish through html using javascript.",
-        mood: "Ok"
-    },
-    {
-        date: "07/28/2025",
-        concept: "Javascript",
-        entry: "We worked on changing information on Martin's Aquarium to load dynamically.",
-        mood: "Ok"
-    },
-]
+let journalEntries = []
+
+const eventHub = document.querySelector(".container")
+
+export const getEntries = () => { 
+    return fetch('http://localhost:3000/notes')
+    .then(response => response.json())
+    .then(parsedEntries => {
+        journalEntries = parsedEntries
+    })
+}
 
 export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
+    const sortedByDate = journalEntries.sort(
         (currentEntry, nextEntry) =>
             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
     )
